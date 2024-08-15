c039ccb37fec17093e9a5c1de4a3dfeb2dad1464
Teja krishna Akkala <102961352+Tejakrishna2003@users.noreply.github.com>
2024-08-16 01:17:05 +0530

    updated node_mudles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import ReactGA from 'react-ga4';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactGA.initialize(`${import.meta.env.VITE_GOOGLE_ANALYTICS_KEY}`);
 
